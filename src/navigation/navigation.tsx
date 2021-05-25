bf3335a7e2b0672add276df989e126aa39062ed3
adam.grund <adam.grund@isdd.sk>
2021-05-25 16:09:40 +0200

    add navigation basic styles

--- src/navigation/navigation.tsx
+++ src/navigation/navigation.tsx
@@ -1,58 +1,16 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useTranslation } from 'react-i18next'
-import {
-  Collapse,
-  DropdownItem,
-  DropdownMenu,
-  DropdownToggle,
-  Nav,
-  Navbar,
-  NavbarBrand,
-  NavbarText,
-  NavbarToggler,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-} from 'reactstrap'
+import { Navbar, NavbarBrand } from 'reactstrap'
 import './navigation.scss'
 
 interface INavigationProps {}
 
 const Navigation: React.FC<INavigationProps> = () => {
-  const [isOpen, setIsOpen] = useState(false)
   const [t] = useTranslation()
 
-  const toggle = () => setIsOpen(!isOpen)
   return (
-    <Navbar color="light" light expand="md">
-      <NavbarBrand href="/">
-        <div>{t('main-title')}</div>
-      </NavbarBrand>
-      <NavbarToggler onClick={toggle} />
-      <Collapse isOpen={isOpen} navbar>
-        <Nav className="mr-auto" navbar>
-          <NavItem>
-            <NavLink href="/components/">Components</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="https://github.com/reactstrap/reactstrap">
-              GitHub
-            </NavLink>
-          </NavItem>
-          <UncontrolledDropdown nav inNavbar>
-            <DropdownToggle nav caret>
-              Options
-            </DropdownToggle>
-            <DropdownMenu right>
-              <DropdownItem>Option 1</DropdownItem>
-              <DropdownItem>Option 2</DropdownItem>
-              <DropdownItem divider />
-              <DropdownItem>Reset</DropdownItem>
-            </DropdownMenu>
-          </UncontrolledDropdown>
-        </Nav>
-        <NavbarText>Simple Text</NavbarText>
-      </Collapse>
+    <Navbar color="primary" light expand="sm">
+      <div className="logo-ba"></div>
     </Navbar>
   )
 }
