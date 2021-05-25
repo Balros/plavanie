8ccf03112269b9b63eeb67a03924172de3f73d0c
adam.grund <adam.grund@isdd.sk>
2021-05-25 15:23:21 +0200

    add reactstrap bootstrap i18next and internet explorer polyfills

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import 'react-app-polyfill/ie11'
+import 'react-app-polyfill/stable'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import React, { Suspense } from 'react'
+import ReactDOM from 'react-dom'
+import './custom.scss'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import i18n from 'i18next'
+import Backend from 'i18next-http-backend'
+import { initReactI18next } from 'react-i18next'
+
+const langWhitelist = ['sk']
+
+i18n
+  // load translation using http -> see /public/locales
+  // learn more: https://github.com/i18next/i18next-http-backend
+  .use(Backend)
+  // pass the i18n instance to react-i18next.
+  .use(initReactI18next)
+  // init i18next
+  // for all options read: https://www.i18next.com/overview/configuration-options
+  .init({
+    fallbackLng: 'sk',
+    lng:
+      localStorage.getItem('i18n.locale') ||
+      (langWhitelist.includes(navigator.language) && navigator.language) ||
+      'sk',
+    debug: false,
+    backend: {
+      loadPath: '/locales/{{lng}}.json',
+    },
+    supportedLngs: langWhitelist,
+    interpolation: {
+      escapeValue: false, // not needed for react as it escapes by default
+    },
+  })
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Suspense fallback={'loading'}>
+      <App />
+    </Suspense>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
--- src/navigation/navigation.tsx
+++ src/navigation/navigation.tsx
@@ -1,8 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import {
+  Collapse,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarText,
+  NavbarToggler,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+} from 'reactstrap'
+import './navigation.scss'
+
 interface INavigationProps {}
 
 const Navigation: React.FC<INavigationProps> = () => {
-  return <div className="primary">Navigation</div>
+  const [isOpen, setIsOpen] = useState(false)
+  const [t] = useTranslation()
+
+  const toggle = () => setIsOpen(!isOpen)
+  return (
+    <Navbar color="light" light expand="md">
+      <NavbarBrand href="/">
+        <div>{t('main-title')}</div>
+      </NavbarBrand>
+      <NavbarToggler onClick={toggle} />
+      <Collapse isOpen={isOpen} navbar>
+        <Nav className="mr-auto" navbar>
+          <NavItem>
+            <NavLink href="/components/">Components</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="https://github.com/reactstrap/reactstrap">
+              GitHub
+            </NavLink>
+          </NavItem>
+          <UncontrolledDropdown nav inNavbar>
+            <DropdownToggle nav caret>
+              Options
+            </DropdownToggle>
+            <DropdownMenu right>
+              <DropdownItem>Option 1</DropdownItem>
+              <DropdownItem>Option 2</DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>Reset</DropdownItem>
+            </DropdownMenu>
+          </UncontrolledDropdown>
+        </Nav>
+        <NavbarText>Simple Text</NavbarText>
+      </Collapse>
+    </Navbar>
+  )
 }
 
 export default Navigation
