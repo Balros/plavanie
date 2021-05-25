968a74755187ef0cc69050230c46ec07ae02edce
adam.grund <adam.grund@isdd.sk>
2021-05-25 13:00:53 +0200

    WIP styles

---
+++ src/navigation/navigation.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+interface INavigationProps {}
+
+const Navigation: React.FC<INavigationProps> = () => {
+  return <div className="primary">Navigation</div>
+}
+
+export default Navigation
