92dc950971d0ee6eedece711b3d49a581bef6158
adam.grund <adam.grund@isdd.sk>
2021-05-25 16:35:41 +0200

    remove unused code from navigation

--- src/navigation/navigation.tsx
+++ src/navigation/navigation.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
-import { useTranslation } from 'react-i18next'
-import { Navbar, NavbarBrand } from 'reactstrap'
+import { Navbar } from 'reactstrap'
+
 import './navigation.scss'
 
 interface INavigationProps {}
 
 const Navigation: React.FC<INavigationProps> = () => {
-  const [t] = useTranslation()
-
   return (
     <Navbar color="primary" light expand="sm">
       <div className="logo-ba"></div>
