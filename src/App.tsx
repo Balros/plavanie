33ce54c9cb5468136dccb883d99d72e6018bcc97
adam.grund <adam.grund@isdd.sk>
2021-05-25 16:35:03 +0200

    add basic landing-page purchase-form

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
 import './App.scss'
 import './assets/styles.scss'
+import { useTranslation } from 'react-i18next'
 import Navigation from './navigation/navigation'
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom'
 
 function App() {
+  const [t] = useTranslation()
+
   return (
     <div className="App">
       <Navigation />
-      <div>content</div>
+      <BrowserRouter>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">{t('landing-page')}</Link>
+            </li>
+            <li>
+              <Link to="/form">{t('purchase-from')}</Link>
+            </li>
+          </ul>
+          <hr />
+          <Switch>
+            <Route exact path="/">
+              {t('landing-page')}
+            </Route>
+            <Route exact path="/form">
+              {t('purchase-from')}
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
     </div>
   )
 }
